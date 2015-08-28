32544b2e96d741b4d3e5129333846852f79c5d5f
smunchi <salah_cse_mbstu@yahoo.com>
2015-08-28 09:48:25 +0600

    this is for level of abstraction

---
+++ 1293.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+ int n,a, b,ans;
+ scanf("%d %d %d", &n, &a, &b);
+ ans = n*a*b*2;
+ printf("%d", ans);
+ return 0;
+}
---
+++ 1877.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+    int firstNight, secondNight;
+
+    scanf("%d", &firstNight);
+    scanf("%d", &secondNight);
+
+    if(firstNight%2==0 || secondNight%2==1) {
+      printf("yes");
+    } else {
+      printf("no");
+    }
+
+    return 0;
+}
